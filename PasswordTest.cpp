c9c7786aa4d89a02ba7efef84077f333502f9f4f
Maxwell Kapral <git@maxkapral.com>
2023-04-12 11:42:29 -0700

    add new tests

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -1,26 +1,60 @@
 /**
  * Unit Tests for Password class
-**/
+ **/
 
-#include <gtest/gtest.h>
 #include "Password.h"
+#include <gtest/gtest.h>
 
 class PracticeTest : public ::testing::Test
 {
-	protected:
-		PracticeTest(){} //constructor runs before each test
-		virtual ~PracticeTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor)
+protected:
+  PracticeTest () {}          // constructor runs before each test
+  virtual ~PracticeTest () {} // destructor cleans up after tests
+  virtual void
+  SetUp ()
+  {
+  } // sets up before each test (after constructor)
+  virtual void
+  TearDown ()
+  {
+  } // clean up after each test, (before destructor)
 };
 
-TEST(PasswordTest, smoke_test)
+TEST (PasswordTest, smoke_test) { ASSERT_TRUE (1 == 1); }
+
+TEST (PasswordTest, empty_string_password)
 {
-    ASSERT_TRUE( 1 == 1 );
+  Password my_password;
+  ASSERT_EQ (0, my_password.count_leading_characters (""));
 }
-TEST(PasswordTest, single_letter_password)
+
+TEST (PasswordTest, single_letter_password)
 {
-    Password my_password;
-	int actual = my_password.count_leading_characters("Z");
-	ASSERT_EQ(1,actual);
+  Password my_password;
+  int actual = my_password.count_leading_characters ("a");
+  ASSERT_EQ (1, actual);
+}
+
+TEST (PasswordTest, multi_char_password)
+{
+  Password my_password;
+  ASSERT_EQ (3, my_password.count_leading_characters ("aaabb"));
+}
+
+TEST (PasswordTest, capital_letter_paritions_password)
+{
+  Password my_password;
+  ASSERT_EQ (1, my_password.count_leading_characters ("aaAabb"));
+}
+
+TEST (PasswordTest, whitespace_password)
+{
+  Password my_password;
+  ASSERT_EQ (3, my_password.count_leading_characters ("\n\n\nab$\r\n"));
+}
+
+TEST (PasswordTest, unicode_password)
+{
+  Password my_password;
+  EXPECT_ANY_THROW (my_password.count_leading_characters ("ßßßc"));
 }
